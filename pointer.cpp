cf5ff49be76ee206714c943b907083ad26e1bda7
Hakim <hmahmoudi470@gmail.com>
2026-02-10 22:35:05 +0100

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string food = "Pizza"; //A string variable 
+    std::string* ptr = &food; //A pointer variable that stores the address fo food
+
+    //Output the value of food 
+    std::cout <<food <<std::endl; 
+
+    // Output the memory address of food 
+    std::cout <<"Refrence: "<<&food <<std::endl; 
+
+    // Output the memory address of food with the pointer 
+    std::cout <<"Pointer: "<<ptr <<std::endl; 
+    return 0; 
+}
